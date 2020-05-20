848814209baea8b849b7651345174e891c11c0e8
suedamarine <Vinny@melvins-mbp.home>
2020-05-20 21:57:29 +0100

    update with some pairwise comparison

--- nephrops.R
+++ nephrops.R
@@ -1,15 +1,23 @@
 # load librarys
 library(tidyverse)
+library(car)
 
 # import some data
 nephrops <- read.csv("data/nephrops.csv")
+
+# check the data
+str(nephrops)
+
+# convert temp to a factor and recode the levels as "T10", "T13", "T16", "T19"
+nephrops$temp <- factor(nephrops$temp, levels = c(10, 13, 16, 19), labels = c("T10", "T13", "T16", "T19"))
+head(nephrops)
 
 # explore data
 nephrops %>%
   ggplot(aes(mass, color = sex, fill = sex)) + geom_histogram(binwidth = 1)
 
 nephrops %>%
-  ggplot(aes(x = sex, y = mass)) + geom_boxplot(aes(color = sex)) + geom_jitter(color = "black")
+  ggplot(aes(x = temp, y = smr, color = sex)) + geom_boxplot() + geom_jitter(color = "black", alpha = 0.2)
 
 nephrops %>% 
   ggplot(aes(x = mass, y = smr, color = sex)) + geom_point() + facet_wrap(.~temp, ncol = 4)
@@ -28,3 +36,17 @@ qnorm(c(0.025, 0.975), 19.3, 5.68)
 # check relationship using a crude linear regression
 lmneph <- lm(smr ~ mass + temp + sex, data = nephrops)
 summary(lmneph)
+
+# try a type III anova
+my_anova <- aov(smr ~ temp * sex, data = nephrops)
+Anova(my_anova, type = "III")
+
+# prepare for a two way comparison
+# first perform an omnibus test to assess main interactions
+anova(lm(smr ~ temp * sex, nephrops))
+
+# now run a pairwise.t.test to test pairwise comparisons between means
+pairwise.t.test(nephrops$smr, nephrops$temp, p.adj = "none")
+
+pairwise.t.test(nephrops$smr, nephrops$sex, p.adj = "none")
+
